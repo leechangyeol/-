4beca625b38e8ff0ff1b7dbcf846e19645df4417
leechangyeol <51622512+leechangyeol@users.noreply.github.com>
2020-12-04 17:21:25 +0900

    Update and rename test.c to 1766.cpp

---
+++ 1766.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <functional>
+#include <queue>
+
+#define MAX 32001
+
+using namespace std;
+
+int n, m;
+vector<int> edge[MAX];
+int edgeN[MAX];
+priority_queue<int,vector<int>,greater<int>> result;
+
+void tolo(void) {
+
+	for (int i = 1; i <= n; i++) {
+		if (edgeN[i] == 0) {
+			result.push(i);
+		}
+	}
+
+	while (!result.empty()) {
+		int x = result.top();
+		result.pop();
+		cout << x << " ";
+		for (int i = 0; i < edge[x].size(); i++) {
+			if (--edgeN[edge[x][i]] == 0) {
+				result.push(edge[x][i]);
+			}
+		}
+		
+	}
+}
+
+int main(void) {
+	cin.tie(NULL); cout.tie(NULL); ios_base::sync_with_stdio(false);
+
+	cin >> n >> m;
+	for (int i = 0; i <= n; i++) {
+		edgeN[i] = 0;
+	}
+	for (int i = 0; i < m; i++) {
+		int a, b;
+		cin >> a >> b;
+		edge[a].push_back(b);
+		edgeN[b]++;
+	}
+	tolo();
+
+}
