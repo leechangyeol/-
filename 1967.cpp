076a87d16d0173d0fcd73cace8e5a7fbf66c5a26
leechangyeol <51622512+leechangyeol@users.noreply.github.com>
2021-01-24 22:51:34 +0900

    Create 1967.cpp

---
+++ 1967.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+using namespace std;
+const int MAX = 1000001;
+int n;
+vector<pair<int, int>> edge[MAX];
+bool visit[MAX];
+int temp_max;
+int leaf;
+void dfs(int node, int value) {
+    visit[node] = 1;
+    for (auto next : edge[node]) {
+        if (visit[next.first])continue;
+        if (temp_max < value + next.second) {
+            leaf = next.first;
+            temp_max = value + next.second;
+        }
+        dfs(next.first, next.second + value);
+    }
+}
+int main(void) {
+    cout.tie(NULL); cin.tie(NULL); ios_base::sync_with_stdio(false);
+    cin >> n;
+
+    for (int i = 1; i < n; i++) {
+        int node, next, value;
+        cin >> node >> next >> value;
+        edge[node].push_back({ next,value });
+        edge[next].push_back({ node,value });
+
+    }
+    dfs(1, 0);
+    memset(visit, 0, size(visit));
+    dfs(leaf, 0);
+    cout << temp_max ;
+}
