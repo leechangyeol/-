f0896d6ca311681315b78a75d02e55a26d724bf3
leechangyeol <51622512+leechangyeol@users.noreply.github.com>
2021-02-07 03:03:53 +0900

    Create 1806.cpp

---
+++ 1806.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int n, m;
+const int MAX = 100000;
+int map[MAX];
+int main(void) {
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) 
+		cin >> map[i];
+	int high = 0, low = 0;
+	int sum = map[0];
+	int ans = MAX + 1;
+	while (low <= high && high < n) {
+		if (sum < m) {
+			sum += map[++high];
+		}
+		else {
+			ans = min(ans, high - low + 1);
+			sum -= map[low++];
+			if (sum >= m)ans = min(ans, high - low + 1);
+		}
+	}
+	if (ans == MAX + 1)cout << 0;
+	else cout << ans;
+}
