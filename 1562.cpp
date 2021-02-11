779df73f1d60fae51b7cb7552c210a5749d31426
leechangyeol <51622512+leechangyeol@users.noreply.github.com>
2021-02-12 02:24:24 +0900

    Update 1562.cpp

--- 1562.cpp
+++ 1562.cpp
@@ -1,1 +1,33 @@
-bitmask
+#include <iostream>
+using namespace std;
+typedef long long ll;
+int n;
+const int MAX = 101;
+const ll MOD = 1000000000;
+ll dp[MAX][10][(1<<10)];
+ll ans;
+int dfs(int num, int bit, int cnt) {
+	if (dp[cnt][num][bit]) return dp[cnt][num][bit];
+	if (cnt == n) {
+		if (bit == (1 << 10) - 1) return 1;
+		else return 0;
+	}
+	ll ret = 0;
+	if (num + 1 < 10)
+		ret += dfs(num + 1, (bit | 1 << (num + 1)), cnt + 1);
+	if (num - 1 >= 0)
+		ret += dfs(num - 1, (bit | 1 << (num - 1)), cnt + 1);
+	
+	return dp[cnt][num][bit] = ret %= MOD;
+
+}
+int main(void) {
+	cin >> n;
+	int bitmask = 0;
+	int test = 0;
+	for (int i = 1; i < 10; i++) {
+		ans += dfs(i, 1 << i, 1);
+		ans %= MOD;
+	}
+	cout << ans;
+}
