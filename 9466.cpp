e285537c772d856d5a1f0020e1cd915c10246b80
leechangyeol <51622512+leechangyeol@users.noreply.github.com>
2021-03-02 00:54:59 +0900

    Update 9466.cpp

--- 9466.cpp
+++ 9466.cpp
@@ -1,38 +1,42 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <cstring>
 using namespace std;
-const int MAX = 100000;
+const int MAX = 100001;
 int list[MAX];
-bool finished[MAX];
+bool visited[MAX];
+bool done[MAX];
 int n;
 int ans;
-vector<int> linked;
 void dfs(int node) {
-	auto it = find(linked.begin(), linked.end(),node);
-	if (it != linked.end()) {
-		ans += it - linked.begin();
-	}
-	else {
-		finished[node] = 1;
-		linked.push_back(node);
+	visited[node] = true;
+
+	if (!visited[list[node]])
 		dfs(list[node]);
+	else if (!done[list[node]]) {
+		for (int i = list[node]; i != node; i = list[i])
+			ans++;
+		ans++;
 	}
+
+	done[node] = true;
 }
 int main(void) {
+	cin.tie(NULL); cout.tie(NULL); ios_base::sync_with_stdio(false);
 	int test;
 	cin >> test;
 	for (int t = 0; t < test; t++) {
 		cin >> n;
 		ans = 0;
-		for (int i = 0; i < n; i++) {
+
+		for (int i = 1; i <= n; i++) {
 			cin >> list[i];
 		}
-		for (auto next : list)
-			if (!finished[next]) { 
-				dfs(next);
-				linked.clear();
-			}
-		cout << ans<<"\n";
+		for (int i = 1;i<=n;i++)
+			if (!visited[i])  
+				dfs(i);
+
+		cout << n - ans << "\n";
+		memset(visited, false, sizeof(visited));
+		memset(done, false, sizeof(done));
 	}
 }
