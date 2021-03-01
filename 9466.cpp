dad6d035adc6ae540b215b85ab4ef29cd3222fa7
leechangyeol <51622512+leechangyeol@users.noreply.github.com>
2021-03-01 23:52:43 +0900

    Update 9466.cpp

--- 9466.cpp
+++ 9466.cpp
@@ -1,2 +1,38 @@
-graph
-dfs
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+const int MAX = 100000;
+int list[MAX];
+bool finished[MAX];
+int n;
+int ans;
+vector<int> linked;
+void dfs(int node) {
+	auto it = find(linked.begin(), linked.end(),node);
+	if (it != linked.end()) {
+		ans += it - linked.begin();
+	}
+	else {
+		finished[node] = 1;
+		linked.push_back(node);
+		dfs(list[node]);
+	}
+}
+int main(void) {
+	int test;
+	cin >> test;
+	for (int t = 0; t < test; t++) {
+		cin >> n;
+		ans = 0;
+		for (int i = 0; i < n; i++) {
+			cin >> list[i];
+		}
+		for (auto next : list)
+			if (!finished[next]) { 
+				dfs(next);
+				linked.clear();
+			}
+		cout << ans<<"\n";
+	}
+}
