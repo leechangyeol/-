d1a1fcb5f4a0fe2cf3d8930357168bedaf1095e5
leechangyeol <51622512+leechangyeol@users.noreply.github.com>
2021-03-04 00:07:43 +0900

    Update 11437.cpp

--- 11437.cpp
+++ 11437.cpp
@@ -5,47 +5,36 @@ using namespace std;
 const int MAX = 50001;
 int n;
 int p[MAX];
-vector<int> ch[MAX];
-void make_ch(int node, int value) {
-	while (node != 0) {
-		ch[node].push_back(value);
-		node = p[node];
+int depth[MAX];
+vector<int> link[MAX];
+void dfs(int node,int level) {
+	for (auto next : link[node]) {
+		if (!p[next]) {
+			p[next] = node;
+			depth[next] = level;
+			dfs(next, level + 1);
+		}
 	}
 }
 int main(void) {
+	cin.tie(NULL); cout.tie(NULL); ios_base::sync_with_stdio(false);
 	cin >> n;
 	int a, b;
 	for (int i = 1; i < n; i++) {
 		cin >> a >> b;
-		if (a == 1) {
-			p[b] = a;
-			make_ch(a, b);
-		}
-		else if (b == 1) {
-			p[a] = b;
-			make_ch(b, a);
-		}
-		else if (p[a] == 0) { 
-			p[a] = b;
-			make_ch(b, a);
-		}
-		else { 
-			p[b] = a;
-			make_ch(a, b);
-		}
+		link[a].push_back(b);
+		link[b].push_back(a);
 	}
+	p[1] = 1;
+	dfs(1, 1);
 	cin >> n;
-	int ans;
+
 	for (int i = 0; i < n; i++) {
 		cin >> a >> b;
-		ans = b;
-		while (1) {
-
-			auto it = find(ch[ans].begin(), ch[ans].end(), a);
-			if (it != ch[ans].end() || ans == a)
-				break;
-			ans = p[ans];
-		}
-		cout << ans << "\n";
+		if (depth[a] > depth[b])swap(a, b);
+		while (depth[a] != depth[b]) b = p[b];
+		while (a != b) a = p[a], b = p[b];
+		cout << a << "\n";
 	}
+	
 }
