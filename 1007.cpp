c2931402eb7c6ecdc75158a88d92c96137fa6d3d
leechangyeol <dcdoc311@naver.com>
2021-03-28 23:41:19 +0900

    Update 1007.cpp

--- 1007.cpp
+++ 1007.cpp
@@ -1,2 +1,32 @@
-dfs
-combination
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+double ans;
+int n, t;
+int ax[21];
+int ay[21];
+bool c[21];
+void combination(int cnt, int index) {
+    if (cnt == n / 2) {
+        double x = 0, y = 0;
+        for (int i = 0; i < n; i++) {
+            if (c[i]) {
+                x -= ax[i];
+                y -= ay[i];
+            }
+            else {
+                x += ax[i];
+                y += ay[i];
+            }
+        }
+        ans = min(ans, sqrt(x*x + y*y));
+        return;
+    }
+    if (index == n) return;
+ 
+    combination(cnt, index + 1);
+    c[index] = true;
+    combination(cnt + 1, index + 1);
+    c[index] = false;
+}
