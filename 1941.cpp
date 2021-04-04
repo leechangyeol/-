784a3a7320d997825c02374326a3e758b287472f
leechangyeol <dcdoc311@naver.com>
2021-04-04 22:31:00 +0900

    Update 1941.cpp

--- 1941.cpp
+++ 1941.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
 
+using namespace std;
+
+int map[5][5];
+bool visited[5][5];
+
+int main(void) {
+	string temp;
+	for (int i = 0; i < 5; i++) {
+		cin >> temp;
+		for (int j = 0; j < 5; j++) {
+			if (temp[j] == 'Y')
+				map[i][j] = 0;
+			else map[i][j] = 1;
+		}
+	}
+
+
+
+}
