b3824d8056551d0ac27aa5201c3985e89a415879
leechangyeol <dcdoc311@naver.com>
2022-02-17 23:10:16 +0900

    Update 1865.cpp

--- 1865.cpp
+++ 1865.cpp
@@ -1,58 +1,65 @@
 #include <iostream>
 #include <vector>
-#include <cstring>
-#include <algorithm>
 using namespace std;
-int t, n, m, w;
-const int MAX = 501;
-const int INF = 987654321;
-vector<pair<int, int>> edge[MAX];
-int flag;
-int dist[MAX][MAX];
-void floyd() {
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			if (i == j) dist[i][j] = 0;
-			else dist[i][j] = INF;
-		}
-		for (auto next : edge[i])
-			dist[i][next.second] = min(dist[i][next.second], next.first);
+
+#define MAX 501
+#define INF 9999999999
+
+vector<pair<int,pair<int, int>>> vec;
+long long dis[MAX];
+
+bool solution() {
+	int n, m, w;
+	cin >> n >> m >> w;
+
+	dis[1] = 0;
+
+	for (int i = 2; i <= n; i++) {
+		dis[i] = INF;
 	}
-	for (int start = 1; start <= n; start++) {
-		for (int end = 1; end <= n; end++) {
-			for (int mid = 1; mid <= n; mid++) {
-				if (dist[start][mid] == INF || dist[mid][start] == INF)continue;
-				dist[start][end] = min(dist[start][end], dist[start][mid] + dist[mid][end]);
-			}
+
+	int s, e, t;
+	for (int i = 0; i < m; i++) {
+		cin >> s >> e >> t;
+		vec.push_back({ s,{e,t} });
+		vec.push_back({ e,{s,t} });
+	}
+
+
+	for (int i = 0; i < w; i++) {
+		cin >> s >> e >> t;
+		vec.push_back({ s,{e, -t} });
+	}
+
+	for (int i = 1; i < n; i++) {
+		for (int j = 0; j < vec.size(); j++) {
+			s = vec[j].first;
+			e = vec[j].second.first;
+			t = vec[j].second.second;
+
+			if (dis[e] > dis[s] + t)dis[e] = dis[s] + t;
 		}
 	}
+
+	for (int i = 0; i < vec.size(); i++) {
+		s = vec[i].first;
+		e = vec[i].second.first;
+		t = vec[i].second.second;
+
+		if (dis[s] == INF)continue;
+		if (dis[e] > dis[s] + t)return true;
+	}
+	return false;
 }
-int main(void) { 
-	cin.tie(NULL); ios_base::sync_with_stdio(false);
-	cin >> t; 
-	for (int test = 0; test < t; test++) {
-		cin >> n >> m >> w;
-		flag = 0;
-		for (int i = 0; i < m; i++) {
-			int a, b, c;
-			cin >> a >> b >> c;
-			edge[a].push_back({ c,b });
-			edge[b].push_back({ c,a });
-		}
-		for (int i = 0; i < w; i++) {
-			int a, b, c;
-			cin >> a >> b >> c;
-			edge[a].push_back({ -c,b });
-		}
-		floyd();
-		for (int i = 1; i <= n; i++) {
-			for (int j = i; j <= n; j++)
-				if (dist[i][j] + dist[j][i] < 0)
-					flag = 1;
-		}
-		if (flag) cout << "YES\n";
+int main(void)
+{
+	int t;
+	cin >> t;
+
+	for (int i = 0; i < t; i++) {
+		vec.clear();
+		if (solution())cout << "YES\n";
 		else cout << "NO\n";
-		for (int i = 1; i <= n; i++)edge[i].clear();
-		memset(dist, 0, sizeof(dist));
 	}
+	return 0;
 }
